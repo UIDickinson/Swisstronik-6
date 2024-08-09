5f752cf770b7620cdf9401f162c6c187d3aae8e8
UI Dickinson <ubongdickinson@gmail.com>
2024-08-09 21:41:06 +0000

    first commit

--- utils/deployed-address.ts
+++ utils/deployed-address.ts
@@ -1,4 +1,4 @@
-export const SWTRProxy = '0xea65D3f80d2253293a64e2f8DdF4CEC401823053'
-export const ProxyAdmin = '0x6a0baE5CF96d14e39FA9F5AdB4fadABb55B22E17'
-export const SWTRImplementation = '0xE4e93a474734E22A4f660EF1fE7aC3fc096E856c'
+export const SWTRProxy = '0x7F6F793164d908adDe7678d4F10a3134A5047959'
+export const ProxyAdmin = '0xabefD3c17421f48A82Ac64777aE69063638Ab27f'
+export const SWTRImplementation = '0x95673B2074Aed36F54a009A089620b2cDe1BDc4E'
     
